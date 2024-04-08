6eb21090ce2c63f75dc4f2abe7b8b0018b453749
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-04-08 18:59:20 +0200

    [feat]: fix stripe config deploy errors

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,11 +1,7 @@
 import Stripe from "stripe";
 import prisma from "@/prisma/client";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+
 const find_package = (plan: string) => {
   switch (plan) {
     case process.env.YEARLY_COMPANY_PACK!: {
